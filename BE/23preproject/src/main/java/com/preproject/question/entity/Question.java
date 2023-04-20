80b2e830abb9bbf8ecf04ebe7dd15de2f339c542
demuu <preasim.kr@gmail.com>
2023-04-21 00:19:19 +0900

    [feat] 김인환: question entity 완료

--- BE/23preproject/src/main/java/com/preproject/question/entity/Question.java
+++ BE/23preproject/src/main/java/com/preproject/question/entity/Question.java
@@ -1,7 +1,33 @@
 package com.preproject.question.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Question {
-    private Long postsId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+    private Long userId;
 
+    @Column(length = 20, nullable = false)
+    private String title;
 
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    private int views;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
 }
