4b0ce0a226b56f57ceb327e6823d63dd2a67b597
윤종협 <99409757+hyobbeee@users.noreply.github.com>
2023-04-21 17:59:08 +0900

    [feat]메인 페이지 제작 및 컴포넌트 추가

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,31 @@
-import React from "react";
+import sprites from "../assets/images/sprites.svg";
 
-export default function Footer() {
-  return <div>푸터</div>;
-}
+const Footer = () => {
+  return (
+    <div className="flex-col">
+      <div className="flex pr-4 py-8 align-top text-soGray-light bg-soGray-footerbg">
+        <div className="flex mx-5">
+          <div
+            className="w-[31px] h-[37px] bg-no-repeat bg-[0_-500px]"
+            style={{ backgroundImage: `url(${sprites})` }}
+          ></div>
+        </div>
+        <div className="mt-2 mb-8 grow">
+          <div className="font-bold mb-4">STACK OVERFLOW</div>
+          <div className="flex mt-2 flex-col text-[hsl(210,8%,60%)] text-xs">
+            <a href="/" className="mb-2">
+              Questions
+            </a>
+            <a href="https://stackoverflow.com/help">Help</a>
+          </div>
+          <div className="mt-4 text-xxs">
+            Site design / logo © 2022 Stack Exchange Inc; user contributions
+            licensed under CC BY-SA. rev 2022.10.28.42999
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { IconSearch, IconAccessibility } from "@stackoverflow/stacks-icons";
 import { fetchUserInfo } from "../utils/fetchLogin";
 import sprites from "../assets/images/sprites.svg";
+import Icon from "../utils/inputIcon";
 
 export default function Header() {
   const [isLogin, setIsLogin] = useState(false);
@@ -43,7 +44,7 @@ export default function Header() {
 
   const LoginGNB = () => {
     return (
-      <div className="flex items-center">
+      <div className="flex items-center ">
         <button
           onClick={onLogoutClick}
           className="px-3 py-1 mx-1 text-gray hover:bg-[#eee] rounded"
@@ -67,8 +68,10 @@ export default function Header() {
   const LogoutGNB = () => {
     return (
       <>
-        <button className="px-3 py-1 mx-2 border rounded text-blue hover:bg-buttonSecondaryHover bg-buttonSecondary border-secondary-200">
-          <Link to="/login">Log in</Link>
+        <button className="px-3 py-1 border rounded hover:bg-color-secondary-700 bg-buttonSecondary border-secondary-200">
+          <Link to="/login" className="text-[hsl(205,47%,42%)]">
+            Log in
+          </Link>
         </button>
         <button className="px-2 py-1 mx-1 text-white border rounded hover:bg-buttonPrimaryHover bg-buttonPrimary border-secondary-300">
           <Link to="/signup">Sign up</Link>
@@ -81,25 +84,42 @@ export default function Header() {
     sessionStorage.removeItem("searchText");
   };
 
-  const inputIcon = (icon) => {
-    return <div dangerouslySetInnerHTML={{ __html: icon }}></div>;
-  };
-
   return (
     <div className="sticky top-0 z-20 flex-col w-full drop-shadow h-[60px] flex-nowrap">
-      <div className="h-1 bg-primary-300"></div>
-      <div className="flex justify-center px-2 py-3 bg-soGray-headerbg">
-        <div className="items-center mx-2 my-1">
+      <div className="h-1 bg-primary-400"></div>
+      <div className="flex justify-center items-center py-2 bg-soGray-headerbg">
+        <div className="mx-2 my-1">
           <Link to="/" onClick={onClickremove}>
             <div
-              className="ml-0 mr-4 w-150 h-30 w-[150px] h-[30px] bg-no-repeat bg-[0_-500px]"
+              className="ml-0 mr-4 w-[150px] h-[30px] bg-no-repeat bg-[0_-500px]"
               style={{ backgroundImage: `url(${sprites})` }}
             ></div>
           </Link>
         </div>
-        <div className="flex items-center px-2 py-1 mx-2 mr-10 bg-white border rounded-md grow border-soGray-light focus:ring-secondary-300">
+
+        <div className="text-sm text-[hsl(210,8%,35%)]">
+          <a
+            href="https://stackoverflow.co/"
+            className="mr-4 hover:bg-[#eee] rounded-full hover:text-black px-1"
+          >
+            About
+          </a>
+          <a
+            href="https://stackoverflow.co/"
+            className="mr-4 hover:bg-[#eee] rounded-full hover:text-black px-1"
+          >
+            Products
+          </a>
+          <a
+            href="https://stackoverflow.co/teams/"
+            className="hover:bg-[#eee] rounded-full hover:text-black px-1"
+          >
+            For Teams
+          </a>
+        </div>
+        <div className="flex items-center mx-2 bg-white border rounded-md grow border-soGray-light focus:ring-secondary-300 h-[36px]">
           <div className="flex mx-2 my-1 text-soGray-icon">
-            {inputIcon(IconSearch)}
+            {Icon(IconSearch)}
           </div>
           <input
             type="text"
@@ -107,6 +127,7 @@ export default function Header() {
             placeholder="Search..."
           />
         </div>
+
         <div>{isLogin ? <LoginGNB /> : <LogoutGNB />}</div>
       </div>
     </div>
