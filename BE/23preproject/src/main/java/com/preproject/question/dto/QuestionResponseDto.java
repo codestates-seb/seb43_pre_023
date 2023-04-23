0f372d304da1615e77f37425de0f3e6274a5e602
demuu <preasim.kr@gmail.com>
2023-04-24 00:58:43 +0900

    [feat] 김인환: QuestionDto 수정

--- BE/23preproject/src/main/java/com/preproject/question/dto/QuestionResponseDto.java
+++ BE/23preproject/src/main/java/com/preproject/question/dto/QuestionResponseDto.java
@@ -1,7 +1,13 @@
 package com.preproject.question.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor
 public class QuestionResponseDto {
     private Long questionId;
     private String title;
@@ -10,13 +16,4 @@ public class QuestionResponseDto {
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 //    private UserResponseDto user;
-
-    public QuestionResponseDto(Long questionId, String title, String content, int views, LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        this.questionId = questionId;
-        this.title = title;
-        this.content = content;
-        this.views = views;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
-    }
 }
